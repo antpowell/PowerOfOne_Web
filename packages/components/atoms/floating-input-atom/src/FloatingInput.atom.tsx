e0d492e430cba3d6e4dee22d2702046613dcd6d3
Powell, Anthony <anthony@anthony-powell.com>
2021-08-09 02:25:48 -0500

    add label for  attribute

--- packages/components/atoms/floating-input-atom/src/FloatingInput.atom.tsx
+++ packages/components/atoms/floating-input-atom/src/FloatingInput.atom.tsx
@@ -19,6 +19,7 @@ export const FloatingInputAtom: FC<FloatingInputAtomProps> = (
 				className='peer h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-rose-600 placeholder-transparent'
 			/>
 			<label
+				htmlFor={props.labelText}
 				className={`absolute transition-all left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:text-sm peer-focus:-top-3.5 peer-focus:text-gray-600`}>
 				{props.labelText}
 			</label>
