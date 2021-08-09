e381638c5946fba1f805f9c5bf559d2f168ec411
Powell, Anthony <anthony@anthony-powell.com>
2021-08-09 02:23:06 -0500

    finish first demo component
    - Floating input label

--- packages/components/atoms/floating-input-atom/src/FloatingInput.atom.tsx
+++ packages/components/atoms/floating-input-atom/src/FloatingInput.atom.tsx
@@ -3,7 +3,7 @@ import React, { FC } from 'react';
 export interface FloatingInputAtomProps {
 	/* Props here */
 	labelText: string;
-	placeHolder: string;
+	placeHolder?: string;
 	type: 'text' | 'password';
 }
 
@@ -11,15 +11,16 @@ export const FloatingInputAtom: FC<FloatingInputAtomProps> = (
 	props: FloatingInputAtomProps,
 ) => {
 	return (
-		<div className=''>
+		<div className='relative'>
 			<input
 				id={props.labelText}
 				name={props.labelText}
 				type={props.type}
-				placeholder={props.placeHolder}
-				className='peer h-10 w-full border-b-2 border-gray-300 text-purple-900 focus:outline-none focus:border-rose-600'
+				placeholder='left empty on purpose for UI to work'
+				className='peer h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-rose-600 placeholder-transparent'
 			/>
-			<label className='peer-placeholder-shown:uppercase'>
+			<label
+				className={`absolute transition-all left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:text-sm peer-focus:-top-3.5 peer-focus:text-gray-600`}>
 				{props.labelText}
 			</label>
 		</div>
